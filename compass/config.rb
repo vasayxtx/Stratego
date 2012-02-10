f24ee546f330f5b5d7216204c12ef2cb1c2fb535
Vasily Tsybenko <vasayxtx@gmail.com>
2012-02-10 21:41:17 +1100

    Add config.rb for compass to rep

---
+++ compass/config.rb
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+require 'compass_twitter_bootstrap'
+
+http_path = "/"
+css_dir = "../public/stylesheets"
+sass_dir = "../views/stylesheets"
+images_dir = "../public/images/"
+output_style = :compressed
